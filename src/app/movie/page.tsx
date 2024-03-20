6c6475c7a965993b04b8dcbc81f7ebd43b4a6074
김태훈 <je64768035@gmail.com>
2024-03-20 13:57:42 +0900

    [test]

---
+++ src/app/movie/page.tsx
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "Movie",
+};
+
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies"
+export default function Movie (){
+
+  return (
+    <div>
+      <h1>movie</h1>
+      
+    </div>
+  )
+}
