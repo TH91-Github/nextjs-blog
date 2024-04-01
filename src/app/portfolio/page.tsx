100351ce31e108d09a2587bd478ddd127b187d5e
김태훈 <je64768035@gmail.com>
2024-04-01 15:19:17 +0900

    tailwind 초기 준비

---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "Portfolio",
+};
+
+
+export default function Portfolio (){
+
+  return (
+    <div>
+      <h1>Portfolio</h1>
+    </div>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,20 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      backgroundSize: ({ theme }) => ({
+        auto: 'auto',
+        cover: 'cover',
+        contain: 'contain',
+        ...theme('spacing')
+      }),
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+    },
+    future: { // 모바일에선 hover 동작하지 않도록
+      hoverOnlyWhenSupported: true,
     },
   },
   plugins: [],
